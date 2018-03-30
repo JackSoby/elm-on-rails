e5d588e726c6d6d26315e653d2ab2864d64201d3
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-03-30 11:12:18 -0400

    build out UI

--- webpack/elm/Main.elm
+++ webpack/elm/Main.elm
@@ -1,4 +1,60 @@
-import Html exposing (text)
+module Main exposing (..)
 
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (value, class, placeholder)
+
+--main function
 main =
-  text "Hello from Elm!"
+    Html.beginnerProgram { view = view
+                        , model = model
+                        , update = update
+                        }
+
+
+type alias Model =
+  { input: String
+  , todos: List String
+  }
+
+
+model : Model
+model =
+    { input = ""
+    , todos = []
+    }
+
+type Msg
+    = AddTodo
+    | HandleTextInput String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddTodo ->
+          { model | input = "", todos = model.input :: model.todos }
+        HandleTextInput nextInput ->
+          {model | input = nextInput }
+
+
+view : Model -> Html Msg
+view model =
+  let
+    renderedTodos = model.todos
+      |> List.map (\todo -> div [class "single-todo"] [text todo, span [class "crud"] [text "Update"], span [class "crud"] [text "Delete"]])
+  in
+  div [class "elm-container"] [
+    div [class"other-body"] [
+      div [ class "form-div"]
+            [
+              form [ ] [
+                input [ placeholder "Write something...", class "input-field", onInput HandleTextInput, value model.input ] []
+                , div [ class "button", onClick AddTodo ] [text "Add Todo"]
+            ]
+          ]
+        ],
+        h1 [class "todo-banner"] [ text "Todos"],
+        div [class "rendered-todos"] renderedTodos
+
+      ]
