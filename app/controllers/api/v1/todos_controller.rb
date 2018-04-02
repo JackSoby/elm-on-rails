f68e2504fe41855d9e02ebd7af1e72c129919c4e
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-04-02 15:01:08 -0400

    mvc

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     todos = Todo.all
-    render json: todos
+    render json: todos.sort.reverse
   end
+  def create
+    newTodo = Todo.new(name: params["name"])
+    if(newTodo.save)
+      render json: newTodo
+    else
+      render json: "todo did not save"
+    end
+  end
+
+  def destroy
+    binding.pry
+  end
+
 end
